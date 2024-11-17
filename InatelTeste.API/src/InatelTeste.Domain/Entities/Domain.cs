80a394a93d81920ea1594c0c8fa7285bc9b862cb
Carlos Daniel <danielpb6293@gmail.com>
2024-11-17 14:20:08 -0300

    criado os models do domain

--- InatelTeste.API/src/InatelTeste.Domain/Entities/Domain.cs
+++ InatelTeste.API/src/InatelTeste.Domain/Entities/Domain.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 
 namespace InatelTeste.Domain.Entities
 {
-    public class University
+    public class Domain
     {
         public int Id { get; set; }
-        public List<WebPage> WebPages { get; set; } = new List<WebPage>();
-        public string Country { get; set; } = null!;
-        public List<Domain> Domains { get; set; } = new List<Domain>();
         public string Name { get; set; } = null!;
+        public int UniversityId { get; set; }
+
+        public University University { get; set; } = null!;
     }
 }
