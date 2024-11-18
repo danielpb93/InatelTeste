946452268f1d83d1d924df6e16d1ce6c9e11360c
Carlos Daniel <danielpb6293@gmail.com>
2024-11-17 21:08:57 -0300

    configurado app component angular

--- InatelTeste.Client/src/app/app-routing.module.ts
+++ InatelTeste.Client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path: '',
+    redirectTo: 'universidades-brasileiras',
+    pathMatch: 'full',
+  },
+  {
+    path: 'universidades-brasileiras',
+    loadChildren: () => import('./university/university.module').then(m => m.UniversityModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
