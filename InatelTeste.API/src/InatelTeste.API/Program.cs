f199539f417899e54614493b9d5e667ffee4a167
Carlos Daniel <danielpb6293@gmail.com>
2024-11-17 23:08:38 -0300

    configurando program

--- InatelTeste.API/src/InatelTeste.API/Program.cs
+++ InatelTeste.API/src/InatelTeste.API/Program.cs
@@ -1,22 +1,54 @@
+using InatelTeste.Application;
+using InatelTeste.Infrastructure;
+using InatelTeste.Infrastructure.seed;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "University API",
+        Description = ""
+    });
+});
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AnyOrigin", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+    var migrationApplied = await dataSeeder.ApplyMigration();
+
+    if (migrationApplied)
+    {
+        await dataSeeder.SeedDatabaseAsync();
+    }
 }
 
-app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AnyOrigin");
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
